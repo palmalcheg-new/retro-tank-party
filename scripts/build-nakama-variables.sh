50d72b739912761826e34b519de18e6c92a1c8ad
David Snopek <dsnopek@gmail.com>
2019-10-28 13:35:55 -0500

    Debugging

--- scripts/build-nakama-variables.sh
+++ scripts/build-nakama-variables.sh
@@ -4,6 +4,9 @@ sed -i -e "s/use_production_nakama = false/use_production_nakama = true/" Main.g
 sed -i -e "s/NAKAMA_HOST/$NAKAMA_HOST/" Main.gd
 sed -i -e "s/'NAKAMA_PORT'/$NAKAMA_PORT/" Main.gd
 
+# Debugging
+cat Main.gd
+
 # Clean-up server key for sed
 NAKAMA_SERVER_KEY=$(echo $NAKAMA_SERVER_KEY | sed -e 's/&/\\\&/')
 
