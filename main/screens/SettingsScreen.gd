eeb9c4d851add35c9378c2ddf87f07cc91cb4113
David Snopek <dsnopek@gmail.com>
2021-03-16 10:53:11 -0500

    Start working on improving the UI theme

--- main/screens/SettingsScreen.gd
+++ main/screens/SettingsScreen.gd
@@ -1,12 +1,14 @@
 extends "res://main/Screen.gd"
 
+onready var music_slider := $Panel/VBoxContainer/GridContainer/MusicSlider
+onready var sound_slider := $Panel/VBoxContainer/GridContainer/SoundSlider
 onready var screenshake_field := $Panel/VBoxContainer/GridContainer/ScreenshakeOptions
-onready var network_relay_field := $Panel/VBoxContainer/GridContainer/NetworkRelayOptions
+onready var network_relay_field:= $Panel/VBoxContainer/GridContainer/NetworkRelayOptions
 
 func _ready() -> void:
-	screenshake_field.add_item("Disabled", 0)
-	screenshake_field.add_item("Enabled", 1)
-	screenshake_field.selected = 1 if GameSettings.use_screenshake else 0
+	screenshake_field.add_item("Enabled", true)
+	screenshake_field.add_item("Disabled", false)
+	screenshake_field.value = GameSettings.use_screenshake
 	
 	network_relay_field.add_item("Auto", OnlineMatch.NetworkRelay.AUTO)
 	network_relay_field.add_item("Forced", OnlineMatch.NetworkRelay.FORCED)
@@ -14,16 +16,16 @@ func _ready() -> void:
 	network_relay_field.selected = GameSettings.use_network_relay
 
 func _show_screen(info: Dictionary = {}) -> void:
-	screenshake_field.grab_focus()
+	music_slider.grab_focus()
 
 func _hide_screen() -> void:
 	GameSettings.save_settings()
 
-func _on_ScreenshakeOptions_item_selected(index: int) -> void:
-	GameSettings.use_screenshake = true if index == 1 else false
+func _on_ScreenshakeOptions_item_selected(value, _index) -> void:
+	GameSettings.use_screenshake = value
 
-func _on_NetworkRelayOptions_item_selected(index: int) -> void:
-	GameSettings.use_network_relay = index
+func _on_NetworkRelayOptions_item_selected(value, _index) -> void:
+	GameSettings.use_network_relay = value
 
 func _on_DoneButton_pressed() -> void:
 	ui_layer.show_screen("TitleScreen")
