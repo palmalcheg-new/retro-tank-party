ad1245d223d43b76f0f44abd114b5c68068da1ea
David Snopek <dsnopek@gmail.com>
2021-04-17 19:13:44 -0500

    Switch to using PackedScene for the pickup scene for consistency

--- src/components/pickups/Pickup.gd
+++ src/components/pickups/Pickup.gd
@@ -3,14 +3,10 @@ class_name Pickup
 
 export (String) var letter := 'P'
 export (int) var rarity := 20
-export (String, FILE, "*.tscn") var pickup_scene := "res://src/objects/pickups/Pickup.tscn"
-
-var _pickup_scene_cached: PackedScene
+export (PackedScene) var pickup_scene: PackedScene = preload("res://src/objects/pickups/Pickup.tscn")
 
 func instance() -> Node2D:
-	if not _pickup_scene_cached:
-		_pickup_scene_cached = load(pickup_scene)
-	var pickup = _pickup_scene_cached.instance()
+	var pickup = pickup_scene.instance()
 	pickup.setup_pickup(self)
 	return pickup
 
