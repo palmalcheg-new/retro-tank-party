7093cb92ed15d7d36b8d632c25d99618d6a7bd50
David Snopek <dsnopek@gmail.com>
2021-05-03 16:11:35 -0500

    Color powerups to indicate if they are a weapon or not

--- src/components/pickups/Pickup.gd
+++ src/components/pickups/Pickup.gd
@@ -4,9 +4,14 @@ class_name Pickup
 export (String) var name := ''
 export (String) var letter := 'P'
 export (int) var rarity := 20
-export (PackedScene) var pickup_scene: PackedScene = preload("res://src/objects/pickups/Pickup.tscn")
+export (PackedScene) var pickup_scene
+
+func get_default_pickup_scene() -> PackedScene:
+	return preload("res://src/objects/pickups/Pickup.tscn")
 
 func instance() -> Node2D:
+	if pickup_scene == null:
+		pickup_scene = get_default_pickup_scene()
 	var pickup = pickup_scene.instance()
 	pickup.setup_pickup(self)
 	return pickup
