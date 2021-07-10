d2d6c03f5cf7de5cebef566a6cf448f7c97b71e5
David Snopek <dsnopek@gmail.com>
2021-07-10 13:54:07 -0500

    Update WebRTC plugin to version 0.5

--- scripts/download-webrtc-plugin.sh
+++ scripts/download-webrtc-plugin.sh
@@ -11,28 +11,18 @@ download() {
 	fi
 }
 
-if [ "$PLATFORM" = "macosx" -o "$PLATFORM" = "all" ]; then
-	download https://github.com/godotengine/webrtc-native/files/3796078/webrtc_0.3_mac_only-with-debug.zip webrtc_0.3_mac_only-with-debug.zip
-	unzip -o -a webrtc_0.3_mac_only-with-debug.zip
-	cp -r webrtc_mac/webrtc/lib webrtc/
-	cp -r webrtc_mac/webrtc_debug/lib webrtc_debug/
-	rm -rf webrtc_0.3_mac_only-with-debug.zip __MACOSX webrtc_mac
-	rm -f webrtc/webrtc.tres webrtc_debug/webrtc_debug.tres
+# Since all platforms are now in the same download, we aren't using $PLATFORM
+# for anything.
+
+if [ "$BUILD" = "release" -o "$BUILD" = "both" ]; then
+	download https://github.com/godotengine/webrtc-native/releases/download/0.5/godot-webrtc-native-release-0.5.zip godot-webrtc-native-release.zip
+	unzip -o -a godot-webrtc-native-release.zip -x '*.tres'
+	rm godot-webrtc-native-release.zip
 fi
 
-if [ "$PLATFORM" != "macosx" ]; then
-	if [ "$BUILD" = "release" -o "$BUILD" = "both" ]; then
-		download https://github.com/godotengine/webrtc-native/releases/download/0.3/webrtc_native_0.3_multi-release.zip webrtc_native_0.3_multi-release.zip
-		unzip -o -a webrtc_native_0.3_multi-release.zip
-		rm webrtc_native_0.3_multi-release.zip
-		rm -f webrtc/webrtc.tres
-	fi
-
-	if [ "$BUILD" = "debug" -o "$BUILD" = "both" ]; then
-		download https://github.com/godotengine/webrtc-native/releases/download/0.3/webrtc_native_0.3_multi-debug.zip webrtc_native_0.3_multi-debug.zip 
-		unzip -o -a webrtc_native_0.3_multi-debug.zip
-		rm webrtc_native_0.3_multi-debug.zip
-		rm -f webrtc_debug/webrtc_debug.tres
-	fi
+if [ "$BUILD" = "debug" -o "$BUILD" = "both" ]; then
+	download https://github.com/godotengine/webrtc-native/releases/download/0.5/godot-webrtc-native-debug-0.5.zip godot-webrtc-native-debug.zip
+	unzip -o -a godot-webrtc-native-debug.zip -x '*.tres'
+	rm godot-webrtc-native-debug.zip
 fi
 
